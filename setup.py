55df312d804b1715d2c0ed21b72f36bf13cb746b
William May <williamcmay@live.com>
2019-04-02 02:33:27 -0400

    Update author information

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1dev',
     description='A Python implementation of the Ice Particle Aggregate Simulator',
     url='https://github.com/ASRCsoft/ipas',
-    author='William May',
+    author='William May, Vanessa Przybylo',
     author_email='williamcmay@live.com',
     packages=['ipas'],
     install_requires=[
